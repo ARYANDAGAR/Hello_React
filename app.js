d0b19bc78c567c85527bcfed04efe293dd13b227
ARYAN DAGAR <aryandagar934@gmail.com>
2024-08-08 07:32:26 +0530

    REACT Learning - P1

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{id : "race"},"Hello React from AryaN DagaR!");
+const run = ReactDOM.createRoot(document.getElementById("first"));
+
+console.log(heading);
+run.render(heading);
